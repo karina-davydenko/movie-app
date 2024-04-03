a32d44f1de4edf0f1be22678803496ea7f5838df
Karina Davydenko <kada61100@gmail.com>
2024-04-04 01:25:43 +0300

    add search suggest

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,22 +1,48 @@
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { useHistory } from '../../shared/hooks/useAddHistory'
+import { useState } from 'react'
+import { useGetSearchByKeywordQuery } from '../../app/store/api/kinopoiskApi'
+import { useDebounce } from '../../shared/hooks/useDebounce'
+import { Box } from '@mui/material'
+import { nanoid } from '@reduxjs/toolkit'
 
 export default function SearchBar() {
   const { search } = useParams()
   const handleOnChange = useHistory()
+  const [inputValue, setInputValue] = useState('')
+  const deb = useDebounce(inputValue, 500)
+  const { data } = useGetSearchByKeywordQuery({ query: deb, page: '1' })
+  const options = data?.films.map(f => f.nameRu)
 
   return (
     <Autocomplete
       freeSolo
       filterOptions={x => x}
-      options={[]}
+      options={options || []}
       onChange={handleOnChange}
+      onInputChange={(e, newValue) => {
+        setInputValue(newValue)
+      }}
       value={search}
+      inputValue={inputValue}
       renderInput={params => <TextField {...params} label='Search movies' />}
-      sx={{ padding: '100px' }}
+      sx={{ margin: '100px' }}
+      renderOption={(props, option) => (
+        <Box
+          sx={{
+            borderRadius: '8px',
+            margin: '5px',
+          }}
+          component='li'
+          {...props}
+          key={nanoid()}
+        >
+          {option}
+        </Box>
+      )}
     />
   )
 }
